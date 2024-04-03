b9040b54d8af4018dd6d9d3eb60a28ec6aae9c50
Sara Ikeda <sara.ikeda10@gmail.com>
2024-04-02 22:04:05 -0300

    Tarefa 4

--- script.js
+++ script.js
@@ -1,43 +1,46 @@
 let b_planet = document.getElementById("b_planet");
 let info_planet = document.getElementById("info_planet");
+let search_planet = document.getElementById("search_planet");
 
-async function Planets(){
-
+async function planets(){
   let p = await fetch('https://swapi.dev/api/planets/?format=json');
   
   let list = await p.json();
   
-  console.log(list.results);
+  console.log(list.results); // Tarefa 1
 
   list.results.forEach(planet => {
   
     let button = document.createElement('button');
 
-    button.innerHTML = `<button>${planet.name}</button>`;
-    button.addEventListener("click", function(){printPlanet(planet)})
-
+    button.innerHTML = `<button>${planet.name}</button>`; // Tarefa 2
+    
+    button.addEventListener("click", function() {showPlanet(planet)}); // Tarefa 3
     b_planet.appendChild(button);
-
+  });
+  
+  document.addEventListener("input", function() { // Tarefa 4
+    showPlanet(list.results.find(planet => planet.name == search_planet.value))
   });
 
 }
 
-function printPlanet(planet){
+function showPlanet(show_planet){
   let info = document.createElement('info');
 
   info.innerHTML = `
     <ul>
-      <li>
-        <strong>Nome: </strong>${planet.name}
+     <li>
+        <strong>Nome: </strong>${show_planet.name}
       </li>
       <li>
-        <strong>Clima: </strong>${planet.climate}
+        <strong>Clima: </strong>${show_planet.climate}
       </li>
       <li>
-        <strong>População: </strong>${planet.population}
+        <strong>População: </strong>${show_planet.population}
       </li>
       <li>
-        <strong>Tipo de terreno: </strong>${planet.terrain}
+        <strong>Tipo de terreno: </strong>${show_planet.terrain}
       </li>
     </ul>
   `;
@@ -47,5 +50,4 @@ function printPlanet(planet){
   info_planet.appendChild(info);
 }
 
-
-Planets();
+planets();
